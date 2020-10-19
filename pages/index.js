e56b002b46df38ba56a7ed0ac49dea39d89d48bc
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-18 20:29:11 -0500

    Estilos en next

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styled from "@emotion/styled";
+
+const Heading = styled.h1`
+  color: red;
+`;
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Inicio</h1>
+    <div>
+      <Heading>Inicio</Heading>
     </div>
   );
 }
