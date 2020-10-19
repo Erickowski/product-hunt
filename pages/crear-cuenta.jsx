39b022200b697e8bf2bc4e32679eb8c05c1028ff
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-18 22:17:50 -0500

    Estructura de crear cuenta

--- pages/crear-cuenta.jsx
+++ pages/crear-cuenta.jsx
@@ -4,6 +4,31 @@ export default function CrearCuenta() {
   return (
     <Layout>
       <h1>Crear Cuenta</h1>
+      <form>
+        <div>
+          <label htmlFor="nombre">Nombre</label>
+          <input
+            type="text"
+            id="nombre"
+            placeholder="Tu nombre"
+            name="nombre"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="Tu email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Tu password"
+            name="password"
+          />
+        </div>
+        <input type="submit" value="Crear cuenta" />
+      </form>
     </Layout>
   );
 }
