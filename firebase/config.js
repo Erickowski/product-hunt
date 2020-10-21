b6d8f62085cc196e1f0ac57ccd2e3088a4ec68cf
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-20 21:03:27 -0500

    Configurar firebase

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCOUhFtX6Vkfa13HXoFrfkL_Fkd5QxE2Jc",
+  authDomain: "product-hunt-31134.firebaseapp.com",
+  databaseURL: "https://product-hunt-31134.firebaseio.com",
+  projectId: "product-hunt-31134",
+  storageBucket: "product-hunt-31134.appspot.com",
+  messagingSenderId: "933316854608",
+  appId: "1:933316854608:web:2d7cf0f921e0b02a13e255",
+};
+
+export default firebaseConfig;
