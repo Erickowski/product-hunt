4880659234855d9f9e852d361b2783fd95c7bf3e
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-20 22:12:59 -0500

    Diseño formulario de crear producto

--- pages/nuevo-producto.jsx
+++ pages/nuevo-producto.jsx
@@ -1,9 +1,130 @@
+import { useState } from "react";
+import { css } from "@emotion/core";
+import Router from "next/router";
+
 import Layout from "../components/layouts/Layout";
+import {
+  Campo,
+  Formulario,
+  InputSubmit,
+  Error,
+} from "../components/ui/Formulario";
+
+import firebase from "../firebase";
+
+import useValidation from "../hooks/useValidation";
+import validarCrearCuenta from "../validacion/validarCrearCuenta";
+
+const STATE_INICIAL = {
+  nombre: "",
+  empresa: "",
+  imagen: "",
+  url: "",
+  descripcion: "",
+};
 
 export default function NuevoProducto() {
+  const [error, setError] = useState(false);
+
+  const {
+    valores,
+    errores,
+    handleSubmit,
+    handleChange,
+    handleBlur,
+  } = useValidation(STATE_INICIAL, validarCrearCuenta, crearCuenta);
+
+  const { nombre, empresa, imagen, url, descripcion } = valores;
+
+  async function crearCuenta() {}
+
   return (
     <Layout>
-      <h1>Nuevo producto</h1>
+      <h1
+        css={css`
+          text-align: center;
+          margin-top: 5rem;
+        `}
+      >
+        Nuevo Producto
+      </h1>
+      <Formulario onSubmit={handleSubmit} noValidate>
+        <fieldset>
+          <legend>Información General</legend>
+
+          <Campo>
+            <label htmlFor="nombre">Nombre</label>
+            <input
+              type="text"
+              id="nombre"
+              placeholder="Tu nombre"
+              name="nombre"
+              value={nombre}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </Campo>
+          {errores.nombre && <Error>{errores.nombre}</Error>}
+
+          <Campo>
+            <label htmlFor="empresa">Empresa</label>
+            <input
+              type="text"
+              id="empresa"
+              placeholder="Tu empresa"
+              name="empresa"
+              value={empresa}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </Campo>
+          {errores.empresa && <Error>{errores.empresa}</Error>}
+
+          <Campo>
+            <label htmlFor="imagen">Imagen</label>
+            <input
+              type="file"
+              id="imagen"
+              name="imagen"
+              value={imagen}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </Campo>
+          {errores.imagen && <Error>{errores.imagen}</Error>}
+
+          <Campo>
+            <label htmlFor="url">URL</label>
+            <input
+              type="url"
+              id="url"
+              name="url"
+              value={url}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </Campo>
+          {errores.url && <Error>{errores.url}</Error>}
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre tu producto</legend>
+          <Campo>
+            <label htmlFor="descripcion">Descripcion</label>
+            <textarea
+              id="descripcion"
+              name="descripcion"
+              value={descripcion}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </Campo>
+          {errores.descripcion && <Error>{errores.descripcion}</Error>}
+        </fieldset>
+
+        {error && <Error>{error}</Error>}
+        <InputSubmit type="submit" value="Crear producto" />
+      </Formulario>
     </Layout>
   );
 }
