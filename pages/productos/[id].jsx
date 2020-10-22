a4970153b23fa9ccd3d454f4ae889b3c772e6ad6
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-21 20:30:16 -0500

    Pagina 404

--- pages/productos/[id].jsx
+++ pages/productos/[id].jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
+import { useRouter } from "next/router";
+
+import { FirebaseContext } from "../../firebase";
+
+import Error404 from "../../components/layouts/404";
+import Layout from "../../components/layouts/Layout";
 
 const Producto = () => {
-  return <h1>Desde id js</h1>;
+  // State del componente
+  const [producto, guardarProducto] = useState({});
+  const [error, guardarError] = useState(false);
+
+  // Routing para obtener el id actual
+  const router = useRouter();
+  const {
+    query: { id },
+  } = router;
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    if (id) {
+      const obtenerProducto = async () => {
+        const productoQuery = await firebase.db.collection("productos").doc(id);
+        const producto = await productoQuery.get();
+        if (producto.exists) {
+          guardarProducto(producto.data());
+        } else {
+          guardarError(true);
+        }
+      };
+      obtenerProducto();
+    }
+  }, [id]);
+
+  return <Layout>{error ? <Error404 /> : <h1>Desde id js</h1>}</Layout>;
 };
 
 export default Producto;
