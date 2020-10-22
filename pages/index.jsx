35cc258e66227cb24168f616b34f08d8e05519c8
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-21 19:43:26 -0500

    Mostrar productos en home

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,45 @@
+import React, { useEffect, useState, useContext } from "react";
+import { FirebaseContext } from "../firebase";
+
 import Layout from "../components/layouts/Layout";
+import DetallesProducto from "../components/layouts/DetallesProducto";
 
 export default function Home() {
+  const [productos, setProductos] = useState([]);
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+      firebase.db
+        .collection("productos")
+        .orderBy("creado", "desc")
+        .onSnapshot(manejarSnapshot);
+    };
+    obtenerProductos();
+  }, []);
+
+  function manejarSnapshot(snapshot) {
+    const productos = snapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+    setProductos(productos);
+  }
+
   return (
     <Layout>
-      <h1>Inicio</h1>
+      <div className="listado-productos">
+        <div className="contenedor">
+          <ul className="bg-white">
+            {productos.map((producto) => (
+              <DetallesProducto key={producto.id} producto={producto} />
+            ))}
+          </ul>
+        </div>
+      </div>
     </Layout>
   );
 }
