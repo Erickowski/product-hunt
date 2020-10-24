1d79baea57a0eda30063b4c73e86bde20707eb42
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-24 15:49:26 -0500

    Custom hook para cambiar orden de productos

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,33 +1,12 @@
-import React, { useEffect, useState, useContext } from "react";
-import { FirebaseContext } from "../firebase";
+import React from "react";
 
 import Layout from "../components/layouts/Layout";
 import DetallesProducto from "../components/layouts/DetallesProducto";
 
+import useProductos from "../hooks/useProductos";
+
 export default function Home() {
-  const [productos, setProductos] = useState([]);
-
-  const { firebase } = useContext(FirebaseContext);
-
-  useEffect(() => {
-    const obtenerProductos = () => {
-      firebase.db
-        .collection("productos")
-        .orderBy("creado", "desc")
-        .onSnapshot(manejarSnapshot);
-    };
-    obtenerProductos();
-  }, []);
-
-  function manejarSnapshot(snapshot) {
-    const productos = snapshot.docs.map((doc) => {
-      return {
-        id: doc.id,
-        ...doc.data(),
-      };
-    });
-    setProductos(productos);
-  }
+  const { productos } = useProductos("creado");
 
   return (
     <Layout>
--- pages/populares.jsx
+++ pages/populares.jsx
@@ -1,9 +1,23 @@
+import React from "react";
+
 import Layout from "../components/layouts/Layout";
+import DetallesProducto from "../components/layouts/DetallesProducto";
+
+import useProductos from "../hooks/useProductos";
 
 export default function Populares() {
+  const { productos } = useProductos("votos");
   return (
     <Layout>
-      <h1>Populares</h1>
+      <div className="listado-productos">
+        <div className="contenedor">
+          <ul className="bg-white">
+            {productos.map((producto) => (
+              <DetallesProducto key={producto.id} producto={producto} />
+            ))}
+          </ul>
+        </div>
+      </div>
     </Layout>
   );
 }
