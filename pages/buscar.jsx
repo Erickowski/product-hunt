43383e09cd002b1334c98319d3a92cd770157339
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-24 16:09:39 -0500

    Buscar y filtrar productos

--- pages/buscar.jsx
+++ pages/buscar.jsx
@@ -1,9 +1,38 @@
+import { useEffect, useState } from "react";
+
 import Layout from "../components/layouts/Layout";
+import { useRouter } from "next/router";
+import DetallesProducto from "../components/layouts/DetallesProducto";
+
+import useProductos from "../hooks/useProductos";
 
 export default function Buscar() {
+  const router = useRouter();
+  const {
+    query: { q },
+  } = router;
+  // Todos los productos
+  const { productos } = useProductos("creado");
+
+  const [resultado, guardarResultado] = useState([]);
+
+  useEffect(() => {
+    const filtro = productos.filter((producto) =>
+      producto.nombre.toLowerCase().includes(q.toLocaleLowerCase())
+    );
+    guardarResultado(filtro);
+  }, [q, productos]);
   return (
     <Layout>
-      <h1>Buscar</h1>
+      <div className="listado-productos">
+        <div className="contenedor">
+          <ul className="bg-white">
+            {resultado.map((producto) => (
+              <DetallesProducto key={producto.id} producto={producto} />
+            ))}
+          </ul>
+        </div>
+      </div>
     </Layout>
   );
 }
